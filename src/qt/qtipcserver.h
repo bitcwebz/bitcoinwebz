8fcfa1058906ddd43ab182b19493fde7bf92d804
bitcwebz <webzbitcoin@gmail.com>
2018-08-07 16:12:48 +0530

    BitcoinWebz

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitcoinWebz-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitcoinWebzURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
